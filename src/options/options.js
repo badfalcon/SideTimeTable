79305a90327f94e4efa48d56ab2cf51808671241
badfalcon <badfalcon610@gmail.com>
2025-01-09 19:17:20 +0900

    refactor: move the localize methods to another js file

--- src/options/options.js
+++ src/options/options.js
@@ -45,7 +45,16 @@ document.addEventListener('DOMContentLoaded', () => {
         const localEventColor = localEventColorInput.value;
         const googleEventColor = googleEventColorInput.value;
 
-        chrome.storage.sync.set({ openTime, closeTime, workTimeColor, breakTimeFixed, breakTimeStart, breakTimeEnd, localEventColor, googleEventColor }, () => {
+        chrome.storage.sync.set({
+            openTime,
+            closeTime,
+            workTimeColor,
+            breakTimeFixed,
+            breakTimeStart,
+            breakTimeEnd,
+            localEventColor,
+            googleEventColor
+        }, () => {
             alert(chrome.i18n.getMessage('settingsSaved'));
         });
     });
@@ -72,34 +81,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
         toggleBreakTimeFields(); // 休憩時間設定を更新
     });
-
-    function replace_i18n(obj, tag) {
-        var msg = tag.replace(/__MSG_(\w+)__/g, function(match, v1) {
-            return v1 ? chrome.i18n.getMessage(v1) : '';
-        });
-
-        if(msg != tag) obj.innerHTML = msg;
-    }
-
-    function localizeHtmlPage() {
-        // Localize using __MSG_***__ data tags
-        var data = document.querySelectorAll('[data-localize]');
-
-        for (var i in data) if (data.hasOwnProperty(i)) {
-            var obj = data[i];
-            var tag = obj.getAttribute('data-localize').toString();
-
-            replace_i18n(obj, tag);
-        }
-
-        // Localize everything else by replacing all __MSG_***__ tags
-        var page = document.getElementsByTagName('html');
-
-        for (var j = 0; j < page.length; j++) {
-            var obj = page[j];
-            var tag = obj.innerHTML.toString();
-
-            replace_i18n(obj, tag);
-        }
-    }
 });
