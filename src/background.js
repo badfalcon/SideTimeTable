bcb90ec51a616cefb3a9c1b9242563bcdba8127f
badfalcon <jun_masuda@beyondjapan.com>
2025-01-16 14:45:35 +0900

    feat: Google login support in browsers other than Google Chrome using Chromium

--- src/background.js
+++ src/background.js
@@ -11,24 +11,34 @@ function getCalendarEvents() {
     console.log("Getting calendar events");
     return new Promise((resolve, reject) => {
         console.log("Requesting auth token");
-        chrome.identity.getAuthToken({interactive: true}, (token) => {
-            console.log("Received auth token", token);
-            if (chrome.runtime.lastError || !token) {
-                console.error("Error getting auth token", chrome.runtime.lastError);
-                reject(chrome.runtime.lastError);
-                return;
-            }
+        const oauth2Manifest = chrome.runtime.getManifest().oauth2
+        const clientId = oauth2Manifest.client_id
+        const scopes = oauth2Manifest.scopes
 
-            console.log("Fetching calendar events");
-            fetch("https://www.googleapis.com/calendar/v3/calendars/primary/events?timeMin=" + new Date(new Date().setHours(0, 0, 0)).toISOString() + "&timeMax=" + new Date(new Date().setHours(23, 59, 59)).toISOString() + "&singleEvents=true&orderBy=startTime", {
-                headers: {
-                    Authorization: "Bearer " + token
+        const authUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${encodeURIComponent(clientId)}&redirect_uri=${encodeURIComponent(chrome.identity.getRedirectURL())}&response_type=token&scope=${encodeURIComponent(scopes.join(' '))}`
+
+        chrome.identity.launchWebAuthFlow(
+            {interactive: true, url: authUrl},
+            (responseUrl) => {
+                const searchParams = new URL(responseUrl.replace(/#/, '?')).searchParams
+                const token = searchParams.get('access_token')
+                console.log("auth token", token);
+                if (chrome.runtime.lastError || !token) {
+                    console.error("Error getting auth token", chrome.runtime.lastError);
+                    reject(chrome.runtime.lastError);
+                    return;
                 }
+
+                console.log("Fetching calendar events");
+                fetch("https://www.googleapis.com/calendar/v3/calendars/primary/events?timeMin=" + new Date(new Date().setHours(0, 0, 0)).toISOString() + "&timeMax=" + new Date(new Date().setHours(23, 59, 59)).toISOString() + "&singleEvents=true&orderBy=startTime", {
+                    headers: {
+                        Authorization: "Bearer " + token
+                    }
+                })
+                    .then(response => response.json())
+                    .then(data => resolve(data.items))
+                    .catch(error => reject(error));
             })
-                .then(response => response.json())
-                .then(data => resolve(data.items))
-                .catch(error => reject(error));
-        });
     });
 }
 
