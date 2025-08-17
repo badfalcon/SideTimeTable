8139e625893f65cb1cc798f1ee120b0377dd1fdc
badfalcon <badfalcon610@gmail.com>
2025-08-18 02:21:48 +0900

    add: support for localizing aria-label attributes and update related input fields

--- src/lib/localize.js
+++ src/lib/localize.js
@@ -10,6 +10,13 @@ function localizeHtmlPage() {
         replace_i18n(element, element.getAttribute('data-localize'));
     });
 
+    // aria-label属性のローカライズ
+    document.querySelectorAll('[data-localize-aria-label]').forEach(element => {
+        const tag = element.getAttribute('data-localize-aria-label');
+        const msg = tag.replace(/__MSG_(\w+)__/g, (match, v1) => chrome.i18n.getMessage(v1) || '');
+        if (msg !== tag) element.setAttribute('aria-label', msg);
+    });
+
     Array.from(document.getElementsByTagName('html')).forEach(element => {
         replace_i18n(element, element.innerHTML);
     });
